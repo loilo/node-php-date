d1a7148938c916a770009f0cf0e6d0c4a8726c5f
Loilo <florian.reuschel@gmx.de>
2017-12-23 23:41:11 +0100

    Fix UTC tests

--- test.js
+++ test.js
@@ -13,8 +13,9 @@ it ('should correctly format local dates', () => {
 })
 
 it ('should correctly format UTC dates', () => {
-	const date = new Date(Date.UTC(2005, 6, 14, 22, 30, 41))
-	assert.equal( format.UTC("F j, Y, g:i a", date), "July 14, 2005, 8:30 pm" )
+	const localDate = new Date(2005, 6, 14, 22, 30, 41)
+	const date = new Date(localDate.getTime() - localDate.getTimezoneOffset() * 60 * 1000)
+	assert.equal( format.UTC("F j, Y, g:i a", date), "July 14, 2005, 10:30 pm" )
 	assert.equal( format.UTC("m.d.y", date), "07.14.05" )
 	assert.equal( format.UTC("j, n, Y", date), "14, 7, 2005" )
 	assert.equal( format.UTC("Ymd", date), "20050714" )
