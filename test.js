7887b2bd08a1ffdf3750a988dfbc5960403ae70a
Florian Reuschel <Florian.Reuschel@gmx.de>
2017-12-23 20:45:07 +0100

    Fix UTC tests

--- test.js
+++ test.js
@@ -1,9 +1,8 @@
 const format = require('./src/index')
 const assert = require('assert')
 
-const date = new Date(2005, 6, 14, 22, 30, 41)
-
 it ('should correctly format local dates', () => {
+	const date = new Date(2005, 6, 14, 22, 30, 41)
 	assert.equal( format("F j, Y, g:i a", date), "July 14, 2005, 10:30 pm" )
 	assert.equal( format("m.d.y", date), "07.14.05" )
 	assert.equal( format("j, n, Y", date), "14, 7, 2005" )
@@ -14,6 +13,7 @@ it ('should correctly format local dates', () => {
 })
 
 it ('should correctly format UTC dates', () => {
+	const date = new Date(Date.UTC(2005, 6, 14, 22, 30, 41))
 	assert.equal( format.UTC("F j, Y, g:i a", date), "July 14, 2005, 8:30 pm" )
 	assert.equal( format.UTC("m.d.y", date), "07.14.05" )
 	assert.equal( format.UTC("j, n, Y", date), "14, 7, 2005" )
