7b75be023d35cb99ccbb59c430edccd7bb300da0
loilo <florian.reuschel@gmx.de>
2019-10-21 15:38:06 +0200

    Improve typings

--- src/date.d.ts
+++ src/date.d.ts
@@ -1,18 +1,18 @@
-declare function formatDate (formatterString: string, date?: Date, locale?: string): string
-
+/**
+ * Format a date with PHP-like tokens
+ *
+ * @param formatterString The format of the outputted date string
+ * @param date            A Date object representing the time to be formatted, defaults to current time
+ */
+declare function formatDate(formatterString: string, date?: Date): string
 declare namespace formatDate {
-    function UTC (formatterString: string, date?: Date, locale?: string): string
-
-    interface ILocalizationData {
-        [lang: string]: {
-            days: string[]
-            daysShort: string[]
-            months: string[]
-            monthsShort: string[]
-        }
-    }
-
-    var localizationData: ILocalizationData
+  /**
+   * Format a date in the UTC timezone with PHP-like tokens
+   *
+   * @param formatterString The format of the outputted date string
+   * @param date            A Date object representing the time to be formatted, defaults to current time
+   */
+  export function UTC(formatterString: string, date?: Date): string
 }
 
 export = formatDate
