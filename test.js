a8fdcbacaf2f29b673cf7e2d775abf42480aee12
loilo <florian.reuschel@gmx.de>
2019-10-21 11:56:07 +0200

    Use jest instead of mocha

--- test.js
+++ test.js
@@ -1,25 +1,32 @@
 const format = require('./src/index')
-const assert = require('assert')
 
-it ('should correctly format local dates', () => {
-	const date = new Date(2005, 6, 14, 22, 30, 41)
-	assert.equal( format("F j, Y, g:i a", date), "July 14, 2005, 10:30 pm" )
-	assert.equal( format("m.d.y", date), "07.14.05" )
-	assert.equal( format("j, n, Y", date), "14, 7, 2005" )
-	assert.equal( format("Ymd", date), "20050714" )
-	assert.equal( format('h-i-s, j-m-y, it is w Day', date), "10-30-41, 14-07-05, 3031 3041 4 Thupm05" )
-	assert.equal( format("D M j G:i:s Y", date), "Thu Jul 14 22:30:41 2005" )
-	assert.equal( format("H:i:s", date), "22:30:41" )
-})
+describe('integration tests', () => {
+  it('should correctly format local dates', () => {
+    const date = new Date(2005, 6, 14, 22, 30, 41)
+    expect(format('F j, Y, g:i a', date)).toBe('July 14, 2005, 10:30 pm')
+    expect(format('m.d.y', date)).toBe('07.14.05')
+    expect(format('j, n, Y', date)).toBe('14, 7, 2005')
+    expect(format('Ymd', date)).toBe('20050714')
+    expect(format('h-i-s, j-m-y, it is w Day', date)).toBe(
+      '10-30-41, 14-07-05, 3031 3041 4 Thupm05'
+    )
+    expect(format('D M j G:i:s Y', date)).toBe('Thu Jul 14 22:30:41 2005')
+    expect(format('H:i:s', date)).toBe('22:30:41')
+  })
 
-it ('should correctly format UTC dates', () => {
-	const localDate = new Date(2005, 6, 14, 22, 30, 41)
-	const date = new Date(localDate.getTime() - localDate.getTimezoneOffset() * 60 * 1000)
-	assert.equal( format.UTC("F j, Y, g:i a", date), "July 14, 2005, 10:30 pm" )
-	assert.equal( format.UTC("m.d.y", date), "07.14.05" )
-	assert.equal( format.UTC("j, n, Y", date), "14, 7, 2005" )
-	assert.equal( format.UTC("Ymd", date), "20050714" )
-	assert.equal( format.UTC('h-i-s, j-m-y, it is w Day', date), "10-30-41, 14-07-05, 3031 3041 4 Thupm05" )
-	assert.equal( format.UTC("D M j G:i:s Y", date), "Thu Jul 14 22:30:41 2005" )
-	assert.equal( format.UTC("H:i:s", date), "22:30:41" )
+  it('should correctly format UTC dates', () => {
+    const localDate = new Date(2005, 6, 14, 22, 30, 41)
+    const date = new Date(
+      localDate.getTime() - localDate.getTimezoneOffset() * 60 * 1000
+    )
+    expect(format.UTC('F j, Y, g:i a', date)).toBe('July 14, 2005, 10:30 pm')
+    expect(format.UTC('m.d.y', date)).toBe('07.14.05')
+    expect(format.UTC('j, n, Y', date)).toBe('14, 7, 2005')
+    expect(format.UTC('Ymd', date)).toBe('20050714')
+    expect(format.UTC('h-i-s, j-m-y, it is w Day', date)).toBe(
+      '10-30-41, 14-07-05, 3031 3041 4 Thupm05'
+    )
+    expect(format.UTC('D M j G:i:s Y', date)).toBe('Thu Jul 14 22:30:41 2005')
+    expect(format.UTC('H:i:s', date)).toBe('22:30:41')
+  })
 })
